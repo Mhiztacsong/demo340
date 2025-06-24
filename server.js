3743fb066b741aae6c92685687ff9830721e075d
Mhiztacsong <joh24038@byui.edu>
2025-06-24 12:05:07 +0100

    inventory by classification

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -28,6 +28,9 @@ app.use(static)
 
 // Index route
 app.get("/", baseController.buildHome)
+
+//Inventory routes
+app.use("/inv", inventoryRoute)
 
 
 /* ***********************
